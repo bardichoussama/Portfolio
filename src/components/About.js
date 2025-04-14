2e06a027073c82551317cbd13927be88f58c58b7
brcOne <amv.0280@gmail.com>
2025-04-14 22:00:30 +0100

    update ui

--- src/components/About.js
+++ src/components/About.js
@@ -10,11 +10,11 @@ const About = () => {
         {/* Description */}
         <div className="desc">
         <p>
-  Hi! I'm <strong>Oussama Bardich</strong>, a full-stack developer with a passion for both <strong>backend</strong> and <strong>frontend</strong> tech. I build backend solutions with <strong>PHP (Laravel)</strong>, <strong>C# (.NET)</strong>, and mobile apps with <strong>Kotlin (Jetpack Compose)</strong>. For the frontend, I create interactive UIs using <strong>React</strong>, <strong>Vue.js</strong>, and <strong>Angular</strong>.
-  <br/><br/>
-  I have extensive experience working with databases such as <strong>MySQL</strong>, <strong>SQL Server</strong>, and <strong>MongoDB</strong>, and I’m proficient in <strong>UI/UX</strong> design, leveraging tools like <strong>CSS</strong>, <strong>Bootstrap</strong>, and <strong>Tailwind</strong> to deliver intuitive and visually appealing user interfaces. Additionally, 
-  <br/><br/>
-  Always learning and pushing myself to grow, I’m ready to build something great with you!
+  Hi! I&apos;m <strong>Oussama Bardich</strong>, a full-stack developer with a passion for both <strong>backend</strong> and <strong>frontend</strong> tech. I build backend solutions with <strong>PHP (Laravel)</strong>, <strong>C# (.NET)</strong>, and mobile apps with <strong>Kotlin (Jetpack Compose)</strong>. For the frontend, I create interactive UIs using <strong>React</strong>, <strong>Vue.js</strong>, and <strong>Angular</strong>.
+  <br /><br />
+  I have extensive experience working with databases such as <strong>MySQL</strong>, <strong>SQL Server</strong>, and <strong>MongoDB</strong>, and I&apos;m proficient in <strong>UI/UX</strong> design, leveraging tools like <strong>CSS</strong>, <strong>Bootstrap</strong>, and <strong>Tailwind</strong> to deliver intuitive and visually appealing user interfaces. Additionally,
+  <br /><br />
+  Always learning and pushing myself to grow, I&apos;m ready to build something great with you!
 </p>
 
 
